f4e9d798be25894d27ec19ca0d774e55a73ddeb9
99kies <1290017556@qq.com>
2022-03-14 23:00:09 +0800

    update dorafactory node add rococo live json

--- init_dev.sh
+++ init_dev.sh
@@ -4,8 +4,8 @@ echo "正在生成chain spec1..."
 echo "正在生成chain spec2..."
 ./target/release/dorafactory-node build-spec --chain rococo-local-parachain-plain.json --raw --disable-default-bootnode > rococo-local-parachain-2000-raw.json
 echo "正在生成genesis..."
-./target/release/dorafactory-node export-genesis-state --chain rococo-local-parachain-2000-raw.json > para-2000-genesis
+./target/release/dorafactory-node export-genesis-state --chain rococo-live-parachain-2078-raw.json > para-2078-genesis
 echo "正在生成wasm..."
-./target/release/dorafactory-node export-genesis-wasm --chain rococo-local-parachain-2000-raw.json > para-2000-wasm    
+./target/release/dorafactory-node export-genesis-wasm --chain rococo-live-parachain-2078-raw.json > para-2078-wasm
 echo "完成！"
 
