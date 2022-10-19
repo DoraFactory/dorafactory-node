534a6d379e9d394c8a2d207f5c2dfbfc1b863552
99kies <1290017556@qq.com>
2022-10-19 18:04:27 +0800

    update spec-version to 31

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -138,7 +138,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("DORA KSM Parachain"),
     impl_name: create_runtime_str!("DORA KSM Parachain"),
     authoring_version: 1,
-    spec_version: 35,
+    spec_version: 31,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
